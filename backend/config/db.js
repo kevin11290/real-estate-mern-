70635410fc8546e82fa71abfc0db3bf92940d709
kevinsimpson1129@gmail.com <kevinsimpson11290@gmail.com>
2023-04-28 10:26:38 +0900

    complete

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.REACT_APP_MONGO_URI);
+        // const conn = await mongoose.connect(process.env.REACT_APP_MONGO_URI);
+        const conn = await mongoose.connect('mongodb://127.0.0.1:27017/Noud63');        
         console.log(`MongoDB Connected: ${conn.connection.host}`.brightMagenta)
     } catch (error) {
         console.log(`Error: ${error.message}`)
